4ddb00b86d3e1c23f46055d505ebabe44abd64aa
James Cherven <jcherven@mail.usf.edu>
2019-01-27 17:40:01 -0500

    Adds model for user schema

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: string,
+    required: true
+  },
+  email: {
+    type: string,
+    required: true
+  },
+  password: {
+    type: string,
+    required: true
+  },
+  avatar: {
+    type: string,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User =mongoose.model('users', UserSchema);
