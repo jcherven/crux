f3b4d2b81e969fc0a40e22f95bb1977a2d8dd62a
James Cherven <jcherven@mail.usf.edu>
2019-02-13 13:55:42 -0500

    fixes redux store middleware config for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -8,11 +8,21 @@ import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  composeEnhancers(
+    applyMiddleware(...middleware)
+  )
 );
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
