7fa9006e45bfc307ce323edb41e147ec946b1857
James Cherven <jcherven@mail.usf.edu>
2019-02-20 00:18:12 -0500

    Adds static landing route to server.js to test deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express');
 
 require('dotenv').config();
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -18,8 +19,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const db = process.env.MONGODB_URI;
+// Serve up static assets for heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
+const db = process.env.MONGODB_URI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then( () => console.log(`======== MongoDB Connected ========\n`))
@@ -33,6 +38,11 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/expression', expression);
+
+//default route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const port = process.env.PORT;
 
