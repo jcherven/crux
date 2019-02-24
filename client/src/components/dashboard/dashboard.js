30eab7629ab80d230475c31333f13f741f72b6a1
James Cherven <jcherven@mail.usf.edu>
2019-02-24 14:51:15 -0500

    Adds basic profile dashboard display

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -2,7 +2,8 @@
  * /client/src/components/dashboard/Dashboard.js
  *******************************************************/
 
-import React, { Component  } from 'react';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -12,12 +13,52 @@ class Dashboard extends Component {
     this.props.getCurrentProfile();
   }
   render() {
-    return (
-      <div>
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
 
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>
+    } else {
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4>DISPLEY PROFEIL</h4>
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">Welcom { user.name }</p>
+            <p>You have not created a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+            </Link>
+          </div>
+        )
+      }
+    }
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 };
 
-export default connect(null, { getCurrentProfile })(Dashboard);
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
